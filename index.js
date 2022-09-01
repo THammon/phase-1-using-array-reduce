b8ec1afa3650daad7d5f56d83663954b9c4e3826
Tristen <tristen.hammon21@gmail.com>
2022-08-31 20:00:25 -0500

    commit tree f299fe876e18d6cae3e45f28438ede166a1c6d99

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((countObj, currentNumber) => {
+  return countObj + currentNumber
+
+
+}, 0)
+
